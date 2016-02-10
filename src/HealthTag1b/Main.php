b29b6716f851c4683dd621c8f2b69ff73617e483
DevPmServer <addoni1@icloud.com>
2016-02-10 08:36:17 +0300

    Update Main.php

--- src/HealthTag1b/Main.php
+++ src/HealthTag1b/Main.php
@@ -24,14 +24,14 @@ class Main extends PluginBase implements Listener{
         	$this->getServer()->getPluginManager()->registerEvents($this, $this);
 $this->getServer()->getScheduler()->scheduleRepeatingTask(new Health($this), 10);
 	}
-}
+
 public function Popup(){
 
       foreach($this->getServer()->getPlayer () as $player){
             $player->setNameTag("§e".$player->getName()."\n§l§2    ".$player->getHealth()."§c/§2".$player->getMaxHealth());
 
 }
-
+}
 class Health extends PluginTask
 {
     public function __construct(Plugin $owner) {
